6348de442f02989bc3c38f5bb473da9f9bb35a87
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-01-04 21:09:55 +0300

    mustache templates implemented, no more static jobs table

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const path = require("path");
+const JOBS = require("./jobs");
+const mustacheExpress = require("mustache-express");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// Configure mustache
+app.set("views", path.join(__dirname, "path"));
+app.set("view engine", "mustache");
+app.engine("mustache", mustacheExpress());
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./pages/index.html"));
+    // res.sendFile(path.join(__dirname, "./pages/index.html"));
+    res.render("index", { jobs: JOBS });
 });
 
 const port = process.env.PORT || 3000;
