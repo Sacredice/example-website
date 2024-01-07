a60e8e4a71a6ef4b4f617f8fb45f13cc345d97a9
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-01-07 19:10:36 +0300

    helperfunction routing fixed

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,9 +1,12 @@
+const path = require("path");
+
 //  Create an asyncronous function to send the email / How to synchronously send mails through nodemailer?
-const sendEmail = async (transporter, mailOptions) => {
+const sendEmail = async (res, transporter, mailOptions) => {
     try{
       // Use await to wait for the sendMail promise to resolve
       const info = await transporter.sendMail(mailOptions);
       console.log('Email sent: ' + info.messageId);
+      res.status(200).sendFile(path.join(__dirname, "pages", "applied.html"));
     } catch (err) {
       console.error(err)
     }
