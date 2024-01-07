eb24e223686b375aea281d591c1349101d52c5dd
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-01-07 18:59:35 +0300

    fixed helper function name conflict

---
+++ src/helperFunctions.js
@@ -0,0 +1,12 @@
+//  Create an asyncronous function to send the email / How to synchronously send mails through nodemailer?
+const sendEmail = async (mailOptions) => {
+    try{
+      // Use await to wait for the sendMail promise to resolve
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Email sent: ' + info.messageId);
+    } catch (err) {
+      console.error(err)
+    }
+  };
+
+  module.exports = sendEmail;
