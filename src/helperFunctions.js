132c02582bd46e70c3f41104e92c7544f5f84e50
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-01-07 19:03:50 +0300

    fixed helper sendEmail function

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,5 +1,5 @@
 //  Create an asyncronous function to send the email / How to synchronously send mails through nodemailer?
-const sendEmail = async (mailOptions) => {
+const sendEmail = async (transporter, mailOptions) => {
     try{
       // Use await to wait for the sendMail promise to resolve
       const info = await transporter.sendMail(mailOptions);
